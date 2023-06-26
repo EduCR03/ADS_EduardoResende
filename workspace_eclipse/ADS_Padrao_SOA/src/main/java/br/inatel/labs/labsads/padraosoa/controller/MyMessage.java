1b3a39e9393d45836710e1dc70c8b04ea67141bb
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2023-06-26 20:31:29 -0300

    Spring Boot

---
+++ workspace_eclipse/ADS_Padrao_SOA/src/main/java/br/inatel/labs/labsads/padraosoa/controller/MyMessage.java
@@ -0,0 +1,18 @@
+package br.inatel.labs.labsads.padraosoa.controller;
+
+public class MyMessage {
+	
+	private String info;
+
+
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+
+	public String getInfo() {
+		return info;
+	}
+
+}
